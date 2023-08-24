66f1e6cbf0c89b695b6fc1c8a91a3187b2c32034
Taylor McGregor <mcgregor.taylor44@gmail.com>
2023-08-24 14:37:36 -0500

    intital commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string pup = "Shadow"; //This is all for a dog
+            int age = 1;
+            char bestLetter = 'T';
+            bool amTired = true; 
+            double pi = 3.14;
+            decimal alot = 4815162342m;
+            Console.WriteLine($"My dog's name is {pup}, he is {age} years old.");
+            Console.WriteLine($"It is {amTired} {bestLetter} is the first letter of my name.");
+            Console.WriteLine($"The two numbers I think about the most are {pi} and {alot}.");
         }
     }
 }
